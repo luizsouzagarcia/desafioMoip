3f9c39ccec28740a6fa3f3d83adce793b5282f75
luizgarcia <luizgarcia@192.168.1.80>
2017-05-15 19:29:38 -0300

    add

---
+++ moip/src/main/java/br/com/luizgarcia/moip/App.java
@@ -0,0 +1,13 @@
+package br.com.luizgarcia.moip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
