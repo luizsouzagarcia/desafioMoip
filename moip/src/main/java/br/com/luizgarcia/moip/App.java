c84ecda341a94fb74390adf63fef2946de54ef4a
luizgarcia <luizgarcia@192.168.1.80>
2017-05-15 22:47:47 -0300

    add

--- moip/src/main/java/br/com/luizgarcia/moip/App.java
+++ moip/src/main/java/br/com/luizgarcia/moip/App.java
@@ -1,13 +1,18 @@
 package br.com.luizgarcia.moip;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- * Hello world!
+ * App de inicialização.
  *
  */
+@EnableAutoConfiguration
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	 SpringApplication.run(App.class, args);
     }
 }
